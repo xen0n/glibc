f738b8f9e2c63772979e26e491ecf055bff28c50
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2016-06-07 12:06:12 +0100

    De-PLTize __stack_chk_fail internal calls within libc.so.
    We use the same assembler-macro trick we use to de-PLTize
    compiler-generated libcalls to memcpy and memset to redirect
    __stack_chk_fail to __stack_chk_fail_local.
    
    v5: New.
    v6: Only do it within the shared library: with __stack_chk_fail_local
        in libc_pic.a now we don't need to worry about calls from inside
        other routines in libc_nonshared.a any more.
    
    	* sysdeps/generic/symbol-hacks.h (__stack_chk_fail): Add internal
    	alias.

--- sysdeps/generic/symbol-hacks.h
+++ sysdeps/generic/symbol-hacks.h
@@ -5,3 +5,9 @@ asm ("memmove = __GI_memmove");
 asm ("memset = __GI_memset");
 asm ("memcpy = __GI_memcpy");
 #endif
+
+/* -fstack-protector generates calls to __stack_chk_fail, which need
+   similar adjustments to avoid going through the PLT.  */
+#if !defined __ASSEMBLER__ && IS_IN (libc) && defined SHARED
+asm ("__stack_chk_fail = __stack_chk_fail_local");
+#endif
